5e729420b4d764c950fde10ea7f80c2c9d7b23ef
keremvarnali <varnalikerem@gmail.com>
2023-12-11 14:09:00 +0300

    Date Format Fixed

--- Question.java
+++ Question.java
@@ -19,14 +19,27 @@ public class Question {
         this.owner = owner;
         long timeDiff = Application.currentTime.getTimeInMillis() - postDate.getTimeInMillis();
         timeDiff /= (24 * 60 * 60 * 1000);
-        if (timeDiff % 365 != 0) {
-            timePassed = String.format("%d years ago", timeDiff % 365);
+        System.out.println(timeDiff);
+        if (timeDiff / 365 == 1) {
+            timePassed = String.format("%d year ago", timeDiff / 365);
         }
-        else if (timeDiff % 30 != 0) {
-            timePassed = String.format("%d days ago", timeDiff % 30);
+        else if (timeDiff / 365 != 0) {
+            timePassed = String.format("%d years ago", timeDiff / 365);
         }
-        else if (timeDiff %7 != 0) {
-            timePassed = String.format("%d weeks ago", timeDiff % 7);
+        else if (timeDiff / 30 == 1) {
+            timePassed = String.format("%d month ago", timeDiff / 30);
+        }
+        else if (timeDiff / 30 != 0) {
+            timePassed = String.format("%d months ago", timeDiff / 30);
+        }
+        else if (timeDiff / 7 == 1) {
+            timePassed = String.format("%d week ago", timeDiff / 7);
+        }
+        else if (timeDiff / 7 != 0) {
+            timePassed = String.format("%d weeks ago", timeDiff / 7);
+        }
+        else if (timeDiff == 1) {
+            timePassed = String.format("%d day ago", timeDiff);
         }
         else if (timeDiff != 0) {
             timePassed = String.format("%d days ago", timeDiff);
@@ -34,11 +47,20 @@ public class Question {
         else {
             timeDiff = Application.currentTime.getTimeInMillis() - postDate.getTimeInMillis();
             timeDiff /= 1000;
-            if (timeDiff % 3600 != 0) {
-                timePassed = String.format("%d hours ago", timeDiff);
+            if (timeDiff / 3600 == 1) {
+                timePassed = String.format("%d hour ago", timeDiff / 3600);
             }
-            else if(timeDiff % 60 != 0) {
-                timePassed = String.format("%d minutes ago", timeDiff);
+            else if (timeDiff / 3600 != 0) {
+                timePassed = String.format("%d hours ago", timeDiff / 3600);
+            }
+            else if(timeDiff / 60 == 1) {
+                timePassed = String.format("%d minute ago", timeDiff / 60);
+            }
+            else if(timeDiff / 60 != 0) {
+                timePassed = String.format("%d minutes ago", timeDiff / 60);
+            }
+            else if(timeDiff == 1) {
+                timePassed = String.format("%d second ago", timeDiff);
             }
             else {
                 timePassed = String.format("%d seconds ago", timeDiff);
